e818f80f7d5cddb1fa57db234dbd874f311faaa8
ChappalleyT <ChappalleyT@gmail.com>
2015-05-07 15:39:27 +0200

    Update manip.py

--- csudoci/html/manip.py
+++ csudoci/html/manip.py
@@ -10,6 +10,11 @@ généré par ``csudoci.html.parser``.
 
 from csudoci.html.htmltree import Ul, Li, ElementList
 from csudoci.html.parser import HTMLTreeParser
+from csudoci.html import htmltree
+from csudoci.html.htmltree import *
+from csudoci.html.template import FileTemplate
+from csudoci.html.parser import *
+from csudoci.ds.stack import Stack
 
 
 def get_links(tree):
@@ -72,3 +77,40 @@ def test():
 
 if __name__ == '__main__':
     test()
+
+def get_elements(tree, selectors):
+    elements = []
+    root = tree
+    
+    if root.tag in selectors:
+        elements.append(tree)
+    else:
+        for c in tree.children:
+            elements += get_elements(c, selectors)
+
+    return elements
+
+
+def table_matieres(tree, level=2):
+    table = Div()
+    selectors = ['h' + str(i + 1) for i in range(level)]  # je sais pas si je peux faire ça comme ça (God help me (/^^)/ )
+    elements = get_elements(tree, selectors) 
+    
+    for element in elements:
+        table > element
+
+    return table
+
+    
+def remove_links(tree):
+
+    if tree.tag == 'a':
+     
+     # enlèver de cet arbre tous les liens, tout en conservant leur texte.
+     ... # I give up
+     
+     
+    return tree
+
+
+
